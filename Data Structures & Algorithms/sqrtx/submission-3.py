8e8a0b76f39a39935540f46cb836246039fb8501
Shreshth-Rajput <161607443+Shreshth-Rajput@users.noreply.github.com>
2026-04-05 23:27:49 +0530

    Add: sqrtx - submission-3

---
+++ Data Structures & Algorithms/sqrtx/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        lp, rp = 1, x // 2
+        if x == 1:
+            return x
+        while lp <= rp:
+            val = (lp + rp) // 2
+            val_sq = val * val
+            if val_sq == x:
+                return val
+            elif val_sq > x:
+                rp = val - 1
+            else:
+                lp = val + 1
+        return lp - 1

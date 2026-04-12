a85ff89aa384af4c2422ce054c287cae72dde017
Shreshth-Rajput <161607443+Shreshth-Rajput@users.noreply.github.com>
2026-04-12 17:35:35 +0530

    Add: n-th-tribonacci-number - submission-4

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-4.py
@@ -0,0 +1,8 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        t0, t1, t2 = 0, 1, 1
+        for i in range(2, n):
+            t0, t1, t2 = t1, t2, t0 + t1 + t2
+        if n <= 2:
+            return 1 if n == 2 else n
+        return t2
